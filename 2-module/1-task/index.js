4d7bba712e3e23a347ca25b4ab80b97651fd59e8
Nikita Basenkov <nikitan9273@gmail.com>
2021-07-04 17:31:18 +0400

    Tasks

---
+++ 2-module/1-task/index.js
@@ -0,0 +1,8 @@
+/**
+ * Клонируем объект
+ * @param {Object} obj - клонируем объект
+ * @returns {Object}
+ */
+function clone(obj) {
+}    
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# learn
-# learn
+# tasks-js-dom-interfaces
+Repo with tasks for JS/DOM/Interfaces
+
+test 
+ 1
+Changes for CI
